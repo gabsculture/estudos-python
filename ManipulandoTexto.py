322f9c084133917d6ada5085e32f2f89f1781589
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2026-04-06 00:15:36 -0300

    Add string manipulation examples in ManipulandoTexto.py

---
+++ ManipulandoTexto.py
@@ -0,0 +1,36 @@
+ #frase 20 caracteres: curso em video python
+ #fatiamento frase[9:13] do caractere ao 9 até o 12 -> vide
+ #frase[9:21] vai do caractere 9 até o 20 -> video python
+ #frase[9:21:2] começa no 9, paro no 21, pulando de dois em dois -> vdopto
+ #frase[:5] começa do caractere 0 e vai até 4 -> curso
+ #frase[15:] começa do quinze e vai até 20 -> python
+#frase[9::3] comeca no 9 e pula de 3 em 3 -> veph[
+#print(""" texto grande""")
+
+#Análise de strings
+#len(frase) comprimento da frase
+#frase.count('o') conta quantas vezes aparece essa letra
+#frase.count('o',0,13) conta quantes vezes aparece a letra do 0 ao 12
+#frase.find('deo') retorna onde ele encontra deo na string
+#'Curso' in frase retorna se existe curso em frase
+
+#Transformação
+#frase.replace('Python', 'Android') troca a palavra python no texto por android
+#frase.upper() transforma a string em maiúsculo
+#frase.lower() transforma a string em minusculo
+#frase.capitalize() deixa só o primeiro caractere em maiusculo
+#frase.title() analisa quantas palavras tem na string e deixa os primeiros caracteres na string
+#frase.strip() remove espaços inúteis no início e final da string
+#frase.rstrip() remove espaços inuteis a direita
+#frase.lstrip() remove espaços inuteis a esquerda
+
+#Divisão
+#frase.split() divide a string considerando os espaços
+
+#Junção
+#'-'.join(frase)  juntar todas as strings em uma string única colocando - nos espaços
+
+
+frase = 'Curso em Vídeo Python'
+print(len(frase[0]))
+
