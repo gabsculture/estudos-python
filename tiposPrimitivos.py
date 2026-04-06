9d8e572b8bc581381d8035f360a68296663741c6
Gabriela Vitória da Rosa Soares <85495852+gabsculture@users.noreply.github.com>
2026-04-06 00:13:17 -0300

    Add basic data type examples in tiposPrimitivos.py

---
+++ tiposPrimitivos.py
@@ -0,0 +1,36 @@
+#INTEIROS
+n1 = int(input('Digite um número:'))
+n2 = int(input('Digite um número:'))
+soma = n1 + n2
+print('A soma entre {} e {} vale {}'.format(n1, n2, soma))
+##qual a diferenca do format, com o + ou ,?
+
+#FLOAT
+n3 = float(input('Digite um valor com ponto flutuante:'))
+print(n3)
+
+#STRING
+frase = str(input('Digite uma frase:'))
+print(frase)
+
+#BOOL
+bool = bool(input('Digite true ou false'))
+print(bool)
+
+#MÉTODO ISNUEMRIC
+
+n = input('Digite algo')
+print(n.isnumeric())
+
+#MÉTODO ISALPHA
+n4 = input('Digite algo')
+print(n4.isnumeric())
+print(n4.isalpha())
+
+#MÉTODO ISALNUM
+n5 = input('Digite algo')
+print(n5.isalnum())
+
+#MÉTODO ISUPPER
+n6 = input('Digite algo')
+print(n6.isupper())
